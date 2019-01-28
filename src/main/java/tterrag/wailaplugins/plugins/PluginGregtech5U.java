6c234bdd12adacc046764fa603ccc7a11d1f97e8
Jason Mitchell <mitchej@gmail.com>
2019-01-27 17:53:57 -0800

    GT5U: Show which side of the transformer being looked at.

--- src/main/java/tterrag/wailaplugins/plugins/PluginGregtech5U.java
+++ src/main/java/tterrag/wailaplugins/plugins/PluginGregtech5U.java
@@ -16,6 +16,11 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import tterrag.wailaplugins.api.Plugin;
+import static mcp.mobius.waila.api.SpecialChars.RESET;
+import static mcp.mobius.waila.api.SpecialChars.GOLD;
+import static mcp.mobius.waila.api.SpecialChars.BLUE;
+import static mcp.mobius.waila.api.SpecialChars.GREEN;
+import static mcp.mobius.waila.api.SpecialChars.RED;
 
 import java.util.List;
 
@@ -42,19 +47,22 @@ public class PluginGregtech5U extends PluginBase
         final TileEntity tile = accessor.getTileEntity();
         MovingObjectPosition pos = accessor.getPosition();
         NBTTagCompound tag = accessor.getNBTData();
+        final int side = (byte)accessor.getSide().ordinal();
 
         final IGregTechTileEntity tBaseMetaTile = tile instanceof IGregTechTileEntity ? ((IGregTechTileEntity) tile) : null;
         final IMetaTileEntity tMeta = tBaseMetaTile != null ? tBaseMetaTile.getMetaTileEntity() : null;
         final BaseMetaTileEntity mBaseMetaTileEntity = tile instanceof  BaseMetaTileEntity ? ((BaseMetaTileEntity) tile) : null;
+        final boolean showTransformer = tMeta instanceof GT_MetaTileEntity_Transformer && getConfig("transformer");
+        final boolean allowedToWork = tag.hasKey("isAllowedToWork") && tag.getBoolean("isAllowedToWork");
 
         if (tMeta != null) {
             String facingStr = "Facing";
-            if (tMeta instanceof GT_MetaTileEntity_Transformer && getConfig("transformer") && tag.hasKey("isAllowedToWork")) {
+            if (showTransformer && tag.hasKey("isAllowedToWork")) {
                 final GT_MetaTileEntity_Transformer transformer = (GT_MetaTileEntity_Transformer)tMeta;
                 currenttip.add(
                     String.format(
                         "%s %d(%dA) -> %d(%dA)",
-                        tag.getBoolean("isAllowedToWork") ? "Step Down" : "Step Up",
+                        (allowedToWork ? (GREEN + "Step Down") : (RED + "Step Up")) + RESET,
                         tag.getLong("maxEUInput"),
                         tag.getLong("maxAmperesIn"),
                         tag.getLong("maxEUOutput"),
@@ -64,7 +72,16 @@ public class PluginGregtech5U extends PluginBase
                 facingStr = tag.getBoolean("isAllowedToWork") ? "Input" : "Output";
             }
             if (mBaseMetaTileEntity != null && getConfig("machineFacing")) {
-                currenttip.add(String.format("%s: %s", facingStr, ForgeDirection.getOrientation(mBaseMetaTileEntity.getFrontFacing()).name()));
+                final int facing = mBaseMetaTileEntity.getFrontFacing();
+                if(showTransformer) {
+                    if((side == facing && allowedToWork) || (side != facing && !allowedToWork)) {
+                        currenttip.add(String.format(GOLD + "Input:" + RESET + " %d(%dA)", tag.getLong("maxEUInput"), tag.getLong("maxAmperesIn")));
+                    } else {
+                        currenttip.add(String.format(BLUE + "Output:" + RESET + " %d(%dA)", tag.getLong("maxEUOutput"), tag.getLong("maxAmperesOut")));
+                    }
+                } else {
+                    currenttip.add(String.format("%s: %s", facingStr, ForgeDirection.getOrientation(facing).name()));
+                }
             }
         }
 
